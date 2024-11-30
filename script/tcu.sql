bb1f330093d6c807169d275c580e8ac5a82e93d5
Ernesto224 <ev402648@gmail.com>
2024-11-29 19:40:04 -0600

    Update tcu.sql

--- script/tcu.sql
+++ script/tcu.sql
@@ -227,9 +227,152 @@ DELIMITER ;
 
 -- crud actividades ********************************************************************************************************************
 
+DELIMITER $$
 
+CREATE PROCEDURE sp_obtenerActividades()
+BEGIN
 
+    SELECT 
+        id,
+        url_archivo,
+        nombre,
+        descripcion,
+        fecha
+    FROM Actividad
+    WHERE eliminado = FALSE;
 
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_insertarActividad(
+    IN p_url_archivo VARCHAR(255),
+    IN p_nombre VARCHAR(255),
+    IN p_descripcion VARCHAR(500),
+    IN p_id_usuario INT
+)
+BEGIN
+    DECLARE usuario_existente INT;
+    DECLARE error_occurred BOOLEAN DEFAULT FALSE;
+
+    -- verificar que el usuario a ingresar exista en la tabla de usuarios, filtrando por id de dicho usuario
+    SELECT COUNT(*) INTO usuario_existente FROM Usuario WHERE id = p_id_usuario;
+
+    IF  usuario_existente < 0 THEN
+        -- mensaje de error si el usuario no existe
+        SELECT 'Ocurrió un error el usuario no existe.' AS mensaje;
+    ELSE
+        BEGIN
+            -- en caso de que todo sea correcto se insertan los datos
+            INSERT INTO Actividad ( url_archivo, nombre, descripcion, fecha, id_usuario, eliminado ) 
+            VALUES ( p_url_archivo, p_nombre, p_descripcion, NOW(), p_id_usuario, FALSE );
+            SELECT 'Actividad ingresada con exito.' AS mensaje;
+        END;
+    END IF;
+    
+    -- Manejo de excepciones
+    IF error_occurred THEN
+        SELECT 'Ocurrió un error al insertar la actividad.' AS mensaje;
+        ROLLBACK;
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_actualizarActividad(
+    IN p_id INT,
+    IN p_url_archivo VARCHAR(255),
+    IN p_nombre VARCHAR(255),
+    IN p_descripcion VARCHAR(500),
+    IN p_id_usuario INT
+)
+BEGIN
+    DECLARE usuario_existente INT;
+    DECLARE error_occurred BOOLEAN DEFAULT FALSE;
+
+    -- verificar que el usuario a ingresar exista en la tabla de usuarios, filtrando por id de dicho usuario
+    SELECT COUNT(*) INTO usuario_existente FROM Usuario WHERE id = p_id_usuario;
+
+    IF  usuario_existente < 0 THEN
+        -- mensaje de error si el usuario no existe
+        SELECT 'Ocurrió un error el usuario no existe.' AS mensaje;
+    ELSE
+        BEGIN
+            -- actualiza los datos de la actividad sobre-escribiendo la informacion
+            UPDATE Actividad
+            SET url_archivo = p_url_archivo,
+                nombre = p_nombre,
+                descripcion = p_descripcion,
+                fecha = NOW(),
+                id_usuario = p_id_usuario
+            WHERE id = p_id;
+
+            -- Verificar si la actualización afectó filas
+            IF ROW_COUNT() > 0 THEN
+                SELECT 'Actualización exitosa.' AS mensaje;
+            ELSE
+                SELECT 'No se realizaron cambios.' AS mensaje;
+            END IF;
+
+        END;
+    END IF;
+    
+    -- Manejo de excepciones
+    IF error_occurred THEN
+        SELECT 'Ocurrió un error al actualizar la actividad.' AS mensaje;
+        ROLLBACK;
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_actualizarActividad(
+    IN p_id INT,
+)
+BEGIN
+    DECLARE actividad_existente INT;
+    DECLARE eliminado_anterior BOOLEAN;
+    DECLARE error_occurred BOOLEAN DEFAULT FALSE;
+
+    -- verificar que la actividad no este ya eliminada
+    SELECT COUNT(*), eliminado INTO actividad_existente, eliminado_anterior FROM Actividad WHERE id = p_id;
+
+    IF  actividad_existente < 0 THEN
+        -- mensaje de error si la actividad no existe
+        SELECT 'La actividad no existe.' AS mensaje;
+    ELSEIF eliminado_anterior = TRUE THEN
+        -- mensaje si la actividad ya fue eliminada
+        SELECT 'El actividad ya está eliminado.' AS mensaje;
+    ELSE
+        BEGIN
+            -- actualiza los datos de la actividad sobre-escribiendo la informacion del valor borrado
+            UPDATE Actividad
+            SET eliminado = TRUE
+            WHERE id = p_id;
+
+            -- Verificar si la actualización afectó correctamente la columna eliminado
+            SELECT 'Actividad eliminada exitosamente.' AS mensaje;
+
+        END;
+    END IF;
+    
+    -- Manejo de excepciones
+    IF error_occurred THEN
+        SELECT 'Ocurrió un error al eliminar la actividad.' AS mensaje;
+        ROLLBACK;
+    END IF;
+
+END$$
+
+DELIMITER ;
 
 -- crud proyectos ********************************************************************************************************************
 
