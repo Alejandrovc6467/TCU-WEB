4ea751552f1eb9c94e6c04b5ae2e698179d6307a
Alejandrovc6467 <alejandrovc6467@gmail.com>
2025-01-24 15:01:45 -0600

    Listo enviar correo en contacto

--- view/contacto.php
+++ view/contacto.php
@@ -1,14 +1,6 @@
 
 <?php
 include('public/header.php');
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
-
 ?>
 
 
@@ -110,105 +102,7 @@ require 'phpmailer/src/SMTP.php';
 </div>
 
 
-
-
-<script>
-
-// Evento de submit del formulario
-document.getElementById('enviarMensajeContacto')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
-   
-    enviarMensaje();
-        
-});
-
-
-function enviarMensaje(){
-    
-    const nombre = document.getElementById("nombre").value.trim();
-    const apellido = document.getElementById("apellido").value.trim();
-    const correo = document.getElementById("correo").value.trim();
-    const telefono = document.getElementById("telefono").value.trim();
-    const mensaje = document.getElementById("mensaje").value.trim();
-
-
-    // Desactivar el botón mientras se procesa la solicitud AJAX
-    document.getElementById('enviarMensajeContacto').querySelector('button[type="submit"]').disabled = true;
-
-    var form_data = {
-        nombre: nombre,
-        apellido: apellido,
-        correo: correo,
-        telefono: telefono,
-        mensaje: mensaje
-    };
-
-    console.log(form_data);
-
-    
-    $.ajax({
-        type: "POST",
-        url: "view/enviarEmailContacto.php",
-        data: form_data,
-        dataType: "json",
-        success: function (response) {
-
-            console.log(response);
-
-            // Habilitar el botón después de recibir la respuesta del servidor
-            document.getElementById('enviarMensajeContacto').querySelector('button[type="submit"]').disabled = false;
-
-            // Verificar si la respuesta es exitosa
-            if (response.status === "success") {
-                Swal.fire({
-                    icon: 'success',
-                    title: '¡Genial!',
-                    text: 'Nos pondremos en contacto contigo pronto.',
-                    confirmButtonColor: '#088cff'
-                });
-
-
-               //limpiar campos del formulario
-                document.getElementById("nombre").value = '';
-                document.getElementById("apellido").value = '';
-                document.getElementById("correo").value = '';
-                document.getElementById("telefono").value = '';
-                document.getElementById("mensaje").value = '';
-
-
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: response.message || 'Ocurrió un error, intenta nuevamente.',
-                    confirmButtonColor: '#088cff'
-                });
-            }
-
-            
-        },
-        error: function (xhr, status, error) {
-
-            console.log(xhr.responseText);
-
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'No se pudo conectar con el servidor. Por favor, intenta más tarde.',
-                confirmButtonColor: '#088cff'
-            });
-
-            // En caso de error, también habilitar el botón nuevamente
-            document.getElementById('enviarMensajeContacto').querySelector('button[type="submit"]').disabled = false;
-        }
-    });
-    
-}
-
-</script>
-
-
+<script src="public/js/enviarEmail.js?7"></script>
 
 
 
