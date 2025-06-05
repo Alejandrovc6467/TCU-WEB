0c340d8534e7a0a8240ac5b23d36fabe91ae28a5
Ernesto224 <ev402648@gmail.com>
2025-06-04 22:53:03 -0600

    Front herramientas

--- view/herramientas.php
+++ view/herramientas.php
@@ -2,14 +2,19 @@
   include('public/header.php');
 ?>
 
-<div class="noticias-container">
-    <br><br>
-    <p>Herramientas usuario</p>
+<div class="container-noticias">
+  <h1 class="noticias-title">Herramientas</h1>
+  <p class="noticias-title-text">Informate de las herramientas más recientes sobre nuestros proyectos y más.</p>
+
+  <div class="subcontainer-noticias">
+    <!-- Contenedor donde se cargarán dinámicamente las noticias -->
+    <div class="noticias-items">
+      <!-- El contenido se añadirá dinámicamente desde JavaScript -->
+    </div>
+  </div>
 </div>
 
-
-
-<script src="public/js/herramientasFrontEnd.js"></script>
+<script src="public/js/herramientasFrontEnd.js?8"></script>
 
 <?php
 include('public/footer.php');
